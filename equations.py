240dcda604a10e767add43c980fe59d66c4e3c73
manoel <manoelbetmann@hotmail.com>
2019-06-19 23:42:33 -0300

    rarity
    mudança na formula

--- equations.py
+++ equations.py
@@ -1,7 +1,7 @@
 from math import*
 from db.db_utils import*
 
-def frequency(all_fingerprints, fingerprints_z):   # recebe todos os SSIDs e todos os SSIDs de um MAC
+def frequency(fingerprints_z, all_fingerprints):   # recebe todos os SSIDs e todos os SSIDs de um MAC
     return len(fingerprints_z)/len(all_fingerprints)
 
 def inter(list1, list2):  # recebe dois conjuntos de SSIDs para ver quais estão em comum
@@ -15,10 +15,11 @@ def inter(list1, list2):  # recebe dois conjuntos de SSIDs para ver quais estão
         return 0
 
 
-def rarity(inter_list):  # recebe a lista da intersection retornada na funcao INTER
+def rarity(mac1, mac2):  # recebe a lista da intersection retornada na funcao INTER
+    intersect = inter(search_ssid_by_mac(mac1), search_ssid_by_mac(mac2))
     result = 0
-    for item in inter_list:
-        result += (log(frequency(get_all_ssids(), search_all_ssid_macs(item)), 10))*-1
+    for item in intersect:
+        result += (log(frequency(search_all_ssid_macs(item), get_all_ssids()), 10))*-1
     return result
 
 
