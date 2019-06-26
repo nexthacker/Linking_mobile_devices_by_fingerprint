746a4775a66f059763c9a06d85f891a1534c54e4
Evele <lemosevele@gmail.com>
2019-06-25 23:22:27 -0300

    grafico de rank ssids

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ from processing import processamento as pro
 from db import db_helper as dbh
 from equations import rarity, modify_adamic, idf, idf_similarity, adamic, jaccard_similarity
 from processing.link_analisis import define_links
+from processing.graph_generation import graph_rank_ssid
 
-num_colestas = 18
+num_colestas = 3
 if __name__ == "__main__":
     print('Iniciando tabelas...')
     dbh.init(drop_tables=False)
     print('Pronto!')
     print("processando coletas...")
-    tf.text_processing(num_colestas)
-    pro.process_entry(num_colestas)
+    #tf.text_processing(num_colestas)
+    #pro.process_entry(num_colestas)
     print("Analizando dados...")
-    define_links()
+    #define_links()
 
+graph_rank_ssid()
--- processing/graph_generation.py
+++ processing/graph_generation.py
@@ -1,11 +1,32 @@
-import numpy
-import pandas as plt
 import matplotlib.pyplot as plt
+from db import db_utils as dbu
+from pylab import *
 
+def graph_rank_ssid():
+    list_rank =  dbu.rank_ssid_by_apearences()
+    list_result = []
+    listX = []
+    listY= []
+    pos = arange(5) + .30
 
-#TODO: most frequent ssid
-#TODO: comparison between equation
-#TODO: rarity between pairs comparison graph
+    cont = 0
+    for i in list_rank[::-1]:
+        list_result.append(i)
+        cont+=1
+        if cont==5:
+           break
+    print(list_result)
 
-def create_df():
-    pass
+    for i in list_result:
+        listX.append(i[0])
+        listY.append(i[1])
+
+    print(listX)
+    print(listY)
+
+    barh(pos, listY[::-1], align='center', color="#b8ff5c")
+    yticks(pos, listX[::-1])
+    xlabel('Frequência')
+    ylabel('SSIDs')
+    title('Frequência dos SSIDs')
+    show()
