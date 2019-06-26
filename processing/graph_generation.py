b6b37192a587af0688669ac7d84b42d84a09d06a
viiniciuspaes <viniciusjosepaes@gmail.com>
2019-06-26 01:06:35 -0300

    Control

--- processing/graph_generation.py
+++ processing/graph_generation.py
@@ -37,34 +37,39 @@ def graph_rank_ssid():
 
 def alg_comparison():
     links = get_links()
-    # jaccard = []
-    adamic = []
-    # mod_adamic = []
+    jaccard = []
+    # adamic = []
+    mod_adamic = []
     # idf = []
-    # idf_similarity = []
+    idf_similarity = []
+    controle = []
 
     for element in links:
-        # jaccard.append(element[0])
-        adamic.append(element[1])
-        # mod_adamic.append(element[2])
+        jaccard.append(element[0])
+        # adamic.append(element[1])
+        mod_adamic.append(element[2])
         # idf.append(element[3])
-        # idf_similarity.append(element[4])
+        idf_similarity.append(element[4])
+        controle.append((element[4] + element[0])*(0.5))
 
-    s_list = list(range(len(adamic)))
+
+    s_list = list(range(len(jaccard)))
 
 
-    # jaccard.sort()
-    adamic.sort()
-    # mod_adamic.sort()
+    jaccard.sort()
+    # adamic.sort()
+    mod_adamic.sort()
     # idf.sort()
-    # idf_similarity.sort()
+    idf_similarity.sort()
+    controle.sort()
 
     fig, ax = plt.subplots()
-    # ax.plot(s_list, jaccard, label="jaccard")
-    ax.plot(s_list, adamic, label="adamic")
-    # ax.plot(s_list, mod_adamic, label="mod_adamic")
+    ax.plot(s_list, jaccard, label="jaccard")
+    # ax.plot(s_list, adamic, label="adamic")
+    ax.plot(s_list, mod_adamic, label="mod_adamic")
     # ax.plot(s_list, idf, label="idf")
-    # ax.plot(s_list, idf_similarity, label="idf_similarity")
+    ax.plot(s_list, idf_similarity, label="idf_similarity")
+    ax.plot(s_list, controle, label="controle")
     title('Comparação de Links')
     xlabel('Link')
     ylabel('Grau do Link')
